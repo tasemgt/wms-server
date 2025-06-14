4558b9add85fc451a1ff5122c158b3f47e67ce10
Michael Tase <tasemgt@gmail.com>
2025-06-14 21:37:11 +0100

    fix token access bug on register

---
+++ services/smsService.js
@@ -0,0 +1,25 @@
+const dotenv = require('dotenv');
+const twilio = require('twilio');
+
+
+dotenv.config();
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioPhone = process.env.TWILIO_PHONE_NUMBER;
+
+const client = new twilio(accountSid, authToken);
+
+function sendSMS(to, message) {
+  client.messages
+    .create({
+      body: message,
+      from: twilioPhone,
+      to: to              // Recipient's phone number in E.164 format, e.g., +1234567890
+    })
+    .then(message => console.log(`Message sent with SID: ${message.sid}`))
+    .catch(err => console.error('Failed to send message:', err));
+}
+
+// Example usage
+sendSMS('+2348062254916', 'Hello from Node.js and Twilio!');
